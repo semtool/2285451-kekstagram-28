cdad550b4d934f75899987ac598ec9674f10f1c7
Sergey Semerikov <sem.tool@yandex.ru>
2023-03-25 15:09:11 +0300

    Правка недочетов

--- js/functions.js
+++ js/functions.js
@@ -109,3 +109,86 @@ function addsSimbol (string, limit, add){
 }
 
 addsSimbol ('qwerty', 4, '0');
+
+
+// const MAX_HASHTAG_COUNT = 5;
+// const VALID_SYMBOLS = /^#[a-za-яё0-9]{1,19}$/i;
+// const TASK_ERROR_TEXT = 'Не правильно заполнены хэштэги';
+
+// const formPopup = document.querySelector('.img-upload__overlay');
+// const uploadImgButton = document.querySelector('#upload-file');
+// const formPopupClose = document.querySelector('#upload-cancel');
+// const FormAddImage = document.querySelector('#upload-select-image');
+// const inputTextHashtags = formPopup.querySelector('.text__hashtags');
+// const inputTextDescription = formPopup.querySelector('.text__description');
+// const hashtagField = document.querySelector('.text__hashtags');
+
+// const pristine = new Pristine(FormAddImage, {
+//   classTo: 'img-upload__field-wrapper',
+//   errorTextParent: 'img-upload__field-wrapper',
+//   errorTextClass: 'img-upload__field-wrapper__eror'
+// });
+
+
+
+// const onEscapeFormClose = (evt) => {
+//   if (evt.key === 'Escape') {
+//     if(inputTextHashtags === document.activeElement || inputTextDescription === document.activeElement) {
+//       evt.stopPropagation();
+//     } else {
+//       evt.preventDefault();
+//       formPopup.classList.add('hidden');
+//       document.body.classList.remove('modal-open');
+//       uploadImgButton.value = '';
+//       FormAddImage.reset();
+//       document.removeEventListener('keydown', onEscapeFormClose);
+//       formPopupClose.removeEventListener('click', onPopupFormClose);
+//     }
+//   }
+// };
+// const onPopupFormClose = () => {
+//   formPopup.classList.add('hidden');
+//   document.body.classList.remove('modal-open');
+//   uploadImgButton.value = '';
+//   formPopup.reset();
+//   formPopupClose.addEventListener('click', onPopupFormClose);
+//   document.addEventListener('keydown', onEscapeFormClose);
+// };
+
+// const onInputUpload = () => {
+//   formPopup.classList.remove('hidden');
+//   document.body.classList.add('modal-open');
+// };
+
+// const isValidTag = (tag) => VALID_SYMBOLS.test(tag);
+
+// const hasValidCount = (tags) => tags.length <= MAX_HASHTAG_COUNT;
+
+// const hasUniqueTags = (tags) => {
+//   const lowerCaseTags = (tags).map((tag) => tag.toLowerCase());
+//   return lowerCaseTags.length === new Set(lowerCaseTags).size;
+// };
+
+// const validateTags = (value) => {
+//   const tags = value
+//     .trim()
+//     .splint(' ')
+//     .filter((tag) => tag.trim().length);
+//   return hasValidCount(tags) && hasUniqueTags(tags) && tags.every(isValidTag);
+// };
+
+// pristine.addValidator (
+//   hashtagField,
+//   validateTags,
+//   TASK_ERROR_TEXT
+// );
+
+// const onFormSubmit = (evt) => {
+//   evt.preventDefault();
+//   pristine.validate();
+// };
+
+// FormAddImage.addEventListener('submit', onFormSubmit);
+// uploadImgButton.addEventListener('change', onInputUpload);
+// formPopupClose.addEventListener('click', onPopupFormClose);
+// document.addEventListener('keydown', onEscapeFormClose);
--- js/load-fhoto-form.js
+++ js/load-fhoto-form.js
@@ -1,17 +1,73 @@
-const button = document.querySelector('.img-upload__label');
-const buttonаа = document.querySelector('.img-upload__overlay ');
+const CORRECT_HASHTAG_SYMBOLS = /^#[a-zа-яё0-9]{1,19}$/i;
+const MAX_HASHTAG_QUANTITY = 5;
+const ERROR_MESSAGE = 'Правильно заполните поле';
 
-button.addEventListener('click',()=>{
-    buttonаа.classList.remove('hidden');
-    console.log(buttonаа);
+const editingForm = document.querySelector('.img-upload__overlay');
+const loadingFileButton = document.querySelector('#upload-file');
+const outputEditingForm = document.querySelector('#upload-cancel');
+const hashtagField = editingForm.querySelector('.text__hashtags');
+const descriptionField = editingForm.querySelector('.text__description');
+
+const mainEditingForm = document.querySelector('#upload-select-image');
+
+const onDocumentKeydown = (evt) => {
+  if (evt.key === 'Escape') {
+    if(hashtagField === document.activeElement || descriptionField === document.activeElement) {
+      return false;
+    }
+    evt.preventDefault();
+    mainEditingForm.reset();
+    closeEditingForm();
+  }
+};
+
+function openEditingForm () {
+  editingForm.classList.remove('hidden');
+  document.body.classList.add('modal-open');
+  document.addEventListener('keydown', onDocumentKeydown);
+}
+
+function closeEditingForm () {
+  editingForm.classList.add('hidden');
+  document.body.classList.remove('modal-open');
+  document.removeEventListener('keydown', onDocumentKeydown);
+  loadingFileButton.value = '';
+  mainEditingForm.reset();
+}
+
+const pristine = new Pristine(mainEditingForm, {
+  classTo: 'img-upload__field-wrapper',
+  errorTextParent: 'img-upload__field-wrapper',
+  errorTextClass: 'img-upload__field-wrapper__error-text',
 });
 
+const isCorrectTag = (tag) => CORRECT_HASHTAG_SYMBOLS.test(tag);
 
+const hasCorrectTagsCount = (tags) => tags.length <= MAX_HASHTAG_QUANTITY;
 
-const wizardForm = document.querySelector('.img-upload__form');
+const hasUniqueTags = (tags) => {
+  const lowerCaseTags = (tags).map((tag) => tag.toLowerCase());
+  return lowerCaseTags.length === new Set(lowerCaseTags).size;
+};
 
-console.log(wizardForm);
+const validateTags = (value) => {
+  const tagsArr = value.trim().split(' ').filter((tag) => tag.trim().length);
+  return hasCorrectTagsCount(tagsArr) && tagsArr.every(isCorrectTag) && hasUniqueTags(tagsArr);
+};
 
-const pristine = new Pristine(wizardForm);
+pristine.addValidator (
+  hashtagField,
+  validateTags,
+  ERROR_MESSAGE
+);
 
-console.log(pristine);
+const onValidData = (evt)=> {
+  evt.preventDefault();
+  if(pristine.validate()) {
+    mainEditingForm.submit();
+  }
+};
+
+loadingFileButton.addEventListener('change', openEditingForm);
+outputEditingForm.addEventListener('click', closeEditingForm);
+mainEditingForm.addEventListener('submit', onValidData);
