bdec6134d7ca4aa78dcd48b733d3cec212b511f8
Sergey Semerikov <sem.tool@yandex.ru>
2023-03-29 17:57:00 +0300

    9.16. Помощь друга

--- js/load-fhoto-form.js
+++ js/load-fhoto-form.js
@@ -2,6 +2,11 @@ const CORRECT_HASHTAG_SYMBOLS = /^#[a-zа-яё0-9]{1,19}$/i;
 const MAX_HASHTAG_QUANTITY = 5;
 const ERROR_MESSAGE = 'Правильно заполните поле';
 
+const DEFAULT_SCALE = 100;
+const STEP_SCALE = 25;
+const MIN_SCALE = 25;
+
+
 const editingForm = document.querySelector('.img-upload__overlay');
 const loadingFileButton = document.querySelector('#upload-file');
 const outputEditingForm = document.querySelector('#upload-cancel');
@@ -9,6 +14,11 @@ const hashtagField = editingForm.querySelector('.text__hashtags');
 const descriptionField = editingForm.querySelector('.text__description');
 
 const mainEditingForm = document.querySelector('#upload-select-image');
+
+const photoPreview = document.querySelector('.img-upload__preview > img ');
+const buttonPhotoBigger = document.querySelector('.scale__control--bigger');
+const buttonPhotoSmaller = document.querySelector('.scale__control--smaller');
+const outputScale = document.querySelector('.scale__control--value');
 
 const onExitFromFormByEsc = (evt) => {
   if (evt.key === 'Escape') {
@@ -32,6 +42,7 @@ function onCloseEditingForm () {
   document.removeEventListener('keydown', onExitFromFormByEsc);
   loadingFileButton.value = '';
   mainEditingForm.reset();
+  onReturnDafaultScale();
 }
 
 const pristine = new Pristine(mainEditingForm, {
@@ -67,6 +78,63 @@ const onValidData = (evt)=> {
   }
 };
 
+let scaleNumber = parseInt(outputScale.value, 10);
+
+function onChangeScale (volue) {
+  outputScale.value = `${volue}%`;
+  photoPreview.style.transform = `scale(${ volue / 100})`;
+}
+
+function onIncreaseStepScale () {
+  if(scaleNumber < DEFAULT_SCALE){
+    scaleNumber += STEP_SCALE;
+    onChangeScale(scaleNumber);
+  }
+}
+
+function onDecreaseStepScale () {
+  if(scaleNumber > MIN_SCALE){
+    scaleNumber -= STEP_SCALE;
+    onChangeScale(scaleNumber);
+  }
+}
+
+function onReturnDafaultScale () {
+  scaleNumber = DEFAULT_SCALE;
+  onChangeScale(DEFAULT_SCALE);
+}
+
 loadingFileButton.addEventListener('change', onOpenEditingForm);
 outputEditingForm.addEventListener('click', onCloseEditingForm);
 mainEditingForm.addEventListener('submit', onValidData);
+
+buttonPhotoBigger.addEventListener('click', onIncreaseStepScale);
+buttonPhotoSmaller.addEventListener('click', onDecreaseStepScale);
+
+
+
+
+
+const Element = document.querySelector('.img-upload__preview');
+const none = document.querySelector('.effects__list');
+const sliderElement = document.querySelector('effect-level__slider');
+const sepia = document.querySelector('#effect-sepia');
+const marvin = document.querySelector('#effect-marvin');
+const phobos = document.querySelector('#effect-phobos');
+const heat = document.querySelector('#effect-heat');
+
+
+// none.addEventListener('click', (evt)=>{
+//     if(evt.target.value){
+//       Element.className = `effects__preview--${evt.target.value}`;
+//     }
+// });
+
+
+noUiSlider.create(sliderElement, {
+  range: {
+    min: 0,
+    max: 100,
+  },
+  start: 80,
+});
