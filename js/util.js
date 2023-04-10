09be511ec31f07de9c8f52894635710b47c1e92f
Sergey Semerikov <sem.tool@yandex.ru>
2023-04-10 19:51:54 +0300

    тест

--- js/util.js
+++ js/util.js
@@ -51,3 +51,4 @@ function debounce (callback, timeoutDelay = 500) {
 const getRandomArrayElement = (elements) => elements[getRandomInteger(0, elements.length - 1)];
 
 export {getRandomInteger, getRandomArrayElement, createRandomIdFromRangeGenerator, showAlert, debounce};
+//  вавава
